3a661480467de7c9a1b9a0c34ef4957699fb1939
Iden Han <idenhan129@gmail.com>
2020-11-16 21:11:26 +0900

    Make a function called isBlockvalid to chekch whether a block is valid or not and Create static validateStructure for this

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
-
-  static calculateBlockHash = (
-    index: number,
-    previousHash: string,
-    timestamp: number,
-    data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(
     index: number,
@@ -58,6 +66,17 @@ const createNewBlock = (data: string): Block => {
   return newBlock;
 }
 
-console.log(createNewBlock("Hi"), createNewBlock("bye"));
+const isBlockValid = (
+  candidateBlock: Block,
+  previousBlock: Block
+): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if ()
+}
 
 export { };
