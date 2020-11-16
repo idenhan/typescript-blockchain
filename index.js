7878f5f4f28d60b6d33f5c5ddb1ae90415452a9f
Iden Han <idenhan129@gmail.com>
2020-11-16 17:46:11 +0900

    test2

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("Hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi("Iden", 30, "male"));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
-const name = "Iden",
-  age = 24,
-  gender = "male";
-
-const sayHi = (name, age, gender) => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
 }
 
-sayHi(name, age, gender);
+console.log(sayHi("Iden", 30, "male"));
 
 export { };
