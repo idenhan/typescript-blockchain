664cd3d0ecf4f858fa2c85939995a9745a0d11bb
Iden Han <idenhan129@gmail.com>
2020-11-16 21:19:23 +0900

    Make a function called getHashforBlock to check validation in isBlockValid function and complete isBlockValid function and make a function called addBlock

--- src/index.ts
+++ src/index.ts
@@ -66,6 +66,8 @@ const createNewBlock = (data: string): Block => {
   return newBlock;
 }
 
+const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
 const isBlockValid = (
   candidateBlock: Block,
   previousBlock: Block
@@ -76,7 +78,17 @@ const isBlockValid = (
     return false;
   } else if (previousBlock.hash !== candidateBlock.previousHash) {
     return false;
-  } else if ()
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
 }
 
 export { };
