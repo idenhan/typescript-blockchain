7ed335d2d9a87530683a8c626cb2269153ef682f
Iden Han <idenhan129@gmail.com>
2020-11-16 17:32:47 +0900

    test

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+const name = "Iden",
+  age = 24,
+  gender = "male";
+
+const sayHi = (name, age, gender) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi(name, age, gender);
+
+export { };
